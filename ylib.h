6f072014dbf20502cfbe91a312df80a9fe35169c
Younghyung Cho <yhcting77@gmail.com>
2011-05-09 15:58:48 +0900

    Add licenses...

--- ylib.h
+++ ylib.h
@@ -1,11 +1,30 @@
+/*****************************************************************************
+ *    Copyright (C) 2011 Younghyung Cho. <yhcting77@gmail.com>
+ *
+ *    This file is part of ylib
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as
+ *    published by the Free Software Foundation either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License
+ *    (<http://www.gnu.org/licenses/lgpl.html>) for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.	If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+
 #ifndef __YLIb_h__
 #define __YLIb_h__
 
-/*
 #ifdef __cplusplus
 extern "C" {
 #endif
-*/
 
 /*
  * Include headers for modules
@@ -28,10 +47,8 @@ extern "C" {
  * Others
  */
 
-/*
 #ifdef __cplusplus
 }
 #endif
-*/
 
 #endif /* __YLIb_h__ */
